05145ecb18152593163105d2cab8224a9524a550
Renato Benatti <renato.benatti7@gmail.com>
2021-04-04 15:29:31 -0300

    criado função para buscar account por id

--- service/account/accountService.go
+++ service/account/accountService.go
@@ -3,8 +3,11 @@ package service
 import (
 	dao "github.com/devbenatti/rbwallet-go/driven/database/dao/account"
 	dto "github.com/devbenatti/rbwallet-go/dto/account"
+	model "github.com/devbenatti/rbwallet-go/model/errors"
 	"github.com/devbenatti/rbwallet-go/model/valueObject"
 )
+
+type AccountNotFoundErr = model.ErrEntityNotFound
 
 type AccountService struct {
 	accountDAO dao.AccountDAO
@@ -20,6 +23,13 @@ func (as *AccountService) Create(a dto.AccountDTO) {
 	as.accountDAO.Create(a)
 }
 
-func (as *AccountService) FindOne(id valueObject.Uuid) *dto.AccountDTO {
-	return as.accountDAO.FindByID(id)
+func (as *AccountService) FindOne(id valueObject.Uuid) (*dto.AccountDTO, error) {
+
+	acc := as.accountDAO.FindByID(id)
+
+	if acc == nil {
+		return nil, model.NewErrEntityNotFound("account not found")
+	}
+
+	return acc, nil
 }
--- service/account/accountService_test.go
+++ service/account/accountService_test.go
@@ -20,7 +20,8 @@ func (ad *AccountDAOSpy) Create(a dto.AccountDTO) {
 }
 
 func (ad *AccountDAOSpy) FindByID(id valueObject.Uuid) *dto.AccountDTO {
-	return &dto.AccountDTO{}
+	acc := dto.NewAccountDTO("59747936-f126-4286-a214-b15b9d9754e5", "12345678912")
+	return &acc
 }
 
 func TestAccountServiceCreate(t *testing.T) {
@@ -35,5 +36,49 @@ func TestAccountServiceCreate(t *testing.T) {
 
 	if !reflect.DeepEqual(dao.receivedDTO, expectedDTO) {
 		t.Errorf("expect '%v' - result '%v'", expectedDTO, dao.receivedDTO)
+	}
+}
+
+func TestAccountServiceFindByID(t *testing.T) {
+	dao := &AccountDAOSpy{}
+
+	id := valueObject.NewUuid("59747936-f126-4286-a214-b15b9d9754e5")
+
+	expectedDTO := dto.NewAccountDTO("59747936-f126-4286-a214-b15b9d9754e5", "12345678912")
+
+	s := NewAccountService(dao)
+
+	acc, _ := s.FindOne(id)
+
+	if !reflect.DeepEqual(acc, &expectedDTO) {
+		t.Errorf("expect '%v' - result '%v'", expectedDTO, acc)
+	}
+}
+
+var _ dao.AccountDAO = (*AccountDAOSpyTwo)(nil)
+
+type AccountDAOSpyTwo struct {
+	receivedDTO dto.AccountDTO
+}
+
+func (ad *AccountDAOSpyTwo) Create(a dto.AccountDTO) {
+	ad.receivedDTO = a
+}
+
+func (ad *AccountDAOSpyTwo) FindByID(id valueObject.Uuid) *dto.AccountDTO {
+	return nil
+}
+
+func TestAccountServiceFindByIDShouldBeReturnErr(t *testing.T) {
+	dao := &AccountDAOSpyTwo{}
+
+	id := valueObject.NewUuid("59747936-f126-4286-a214-b15b9d9754e5")
+
+	s := NewAccountService(dao)
+
+	_, err := s.FindOne(id)
+
+	if _, ok := err.(*AccountNotFoundErr); !ok {
+		t.Errorf("error should be type of AccountNotFoundErr")
 	}
 }
