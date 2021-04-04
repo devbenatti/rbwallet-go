7dd5f5f09ad24d7bb5dda12b6792a2956ce70c98
Renato Benatti <renato.benatti7@gmail.com>
2021-04-04 15:30:15 -0300

    criado configuração para ler variaveis de ambiente

---
+++ config/config.go
@@ -0,0 +1,30 @@
+package config
+
+import (
+	"log"
+	"os"
+
+	"github.com/joho/godotenv"
+)
+
+type Config struct {
+	DBUser   string
+	DBPasswd string
+	DBAddr   string
+	DBName   string
+}
+
+func Load() *Config {
+	err := godotenv.Load(".env")
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	return &Config{
+		DBUser:   os.Getenv("DATABASE_USER"),
+		DBPasswd: os.Getenv("DATABASE_PASS"),
+		DBAddr:   os.Getenv("DATABASE_HOST"),
+		DBName:   os.Getenv("DATABASE_NAME"),
+	}
+}
